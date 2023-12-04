9cee703c9616784076a32af2c6e8bdfabcf81545
Felipe Quiroz <47114103+felipe-quirozlara@users.noreply.github.com>
2023-12-04 12:15:33 -0300

    static file correction

--- config/urls.py
+++ config/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api/', include('users.urls')),
 ] 
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
